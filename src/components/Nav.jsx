5206df36f8c8b53b0ceb1e491f41627c801ff37d
Karl <toom.karl42@gmail.com>
2023-09-19 16:39:02 +0300

    created a simple sidebar using react-pro-sidebar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,22 @@
 import React, { useRef, useState } from "react";
 import logo from "../assets/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 function Nav() {
+  const sideBarRef = useRef(null);
+  function toggleSideBar() {
+    const sideElement = sideBarRef.current;
+    if (sideElement) {
+      sideElement.classList.toggle("side--visible");
+    }
+  }
   return (
     <>
       <nav>
         <div className="nav__wrapper">
           <div className="nav__left">
-            <button className="menu__btn" >
+            <button className="menu__btn" onClick={toggleSideBar}>
               <FontAwesomeIcon
                 className="menu__btn--img"
                 icon="fa-solid fa-bars"
@@ -22,6 +30,71 @@ function Nav() {
           </div>
         </div>
       </nav>
+      <Sidebar className="sidebar" ref={sideBarRef}>
+        <Menu>
+          <span>Exercises per muscle group</span>
+          <SubMenu label="Chest">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <SubMenu label="Shoulders">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <SubMenu label="Biceps">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <SubMenu label="Triceps">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <SubMenu label="Back">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <SubMenu label="Quads">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <SubMenu label="Hamstring">
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+            <MenuItem> Bench Press </MenuItem>
+            <MenuItem> cable flyes </MenuItem>
+            <MenuItem> Incline Dumbbell Press </MenuItem>
+          </SubMenu>
+          <span>NUTRITION</span>
+          <MenuItem>Foods for Bulking</MenuItem>
+          <MenuItem>Foods for Cutting</MenuItem>
+        </Menu>
+      </Sidebar>
+      ;
     </>
   );
 }
