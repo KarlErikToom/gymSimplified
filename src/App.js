0033e2bb5c169b94193df2e3d700eb846fa0ee30
Karl <toom.karl42@gmail.com>
2023-09-20 15:51:34 +0300

    realized i messed up putting the sidebar inside the nav, going to restart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Router>
+        <div className="stuff">
+
+        <Nav />
+        <Routes>
+          <Route path="/"  element={<Landing />}/>
+        </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,85 +1,32 @@
 import React, { useRef, useState } from "react";
 import logo from "../assets/logo.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { exerciseData as allExerciseData } from "../Data";
 
 function Nav() {
-  const sideBarRef = useRef(null);
-  function toggleSideBar() {
-    const sideElement = sideBarRef.current;
-    if (sideElement) {
-      sideElement.classList.toggle("side--visible");
-    }
-  }
-  const [exercises, setExercises] = useState(allExerciseData);
   return (
     <>
-      <nav>
-        <div className="nav__wrapper">
-          <div className="nav__left">
-            <button className="menu__btn" onClick={toggleSideBar}>
-              <FontAwesomeIcon
-                className="menu__btn--img"
-                icon="fa-solid fa-bars"
-              />
-            </button>
-            <img src={logo} alt="" className="nav__logo" />
-          </div>
-          <div className="nav__buttons">
-            <button className="btn">Sign up</button>
-            <button className="btn">Log in</button>
-          </div>
-        </div>
-      </nav>
-      <Sidebar className="sidebar" ref={sideBarRef}>
+      <Sidebar>
         <Menu>
-          <div className="exercise__menu">
-            <span>Exercises per muscle group</span>
-            <SubMenu label="Chest">
-              {exercises.Chest.map((exercise) => (
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-            <SubMenu label="Shoulders">
-              {exercises.Shoulders.map((exercise) =>(
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-            <SubMenu label="Biceps">
-              {exercises.Biceps.map((exercise) =>(
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-            <SubMenu label="Triceps">
-              {exercises.Triceps.map((exercise) =>(
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-            <SubMenu label="Back">
-              {exercises.Back.map((exercise) =>(
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-            <SubMenu label="Quads">
-              {exercises.Quads.map((exercise) =>(
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-            <SubMenu label="Hamstring">
-              {exercises.Hamstrings.map((exercise) =>(
-                <MenuItem key={exercise.id}>{exercise.name}</MenuItem>
-              ))}
-            </SubMenu>
-          </div>
-          <div className="exercise__menu">
-            <span>NUTRITION</span>
-            <MenuItem>Foods for Bulking</MenuItem>
-            <MenuItem>Foods for Cutting</MenuItem>
-          </div>
+          <SubMenu label='stuff'>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+          </SubMenu>
+          <SubMenu label='stuff'>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+            <MenuItem>me</MenuItem>
+          </SubMenu>
         </Menu>
       </Sidebar>
-      ;
     </>
   );
 }
