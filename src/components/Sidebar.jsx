8a0d876a176e2d65483077eca1cf5a77a89f83d7
Karl <toom.karl42@gmail.com>
2023-10-03 16:31:35 +0300

    added the free and users only sections back into the sidemenu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,28 +6,42 @@ import { Link } from "react-router-dom";
 function Sidebar({ isOpen }) {
   return (
     <Side className={`sidebar ${isOpen ? "open" : "closed"}`}>
-      <Menu>
-        <div className="workouts">
-          <div className="workouts__wrapper">
-            <div className="workouts__label">
-              <span>Muscle Groups</span>
-            </div>
-            {Object.entries(exerciseData).map(([muscleGroup, exercises]) => (
-              <SubMenu label={muscleGroup} key={muscleGroup}>
-                {exercises.map((exercise) => (
+    <Menu>
+      <div className="workouts">
+        <div className="workouts__wrapper">
+          <div className="workouts__label">
+            <span>Muscle Groups</span>
+          </div>
+          {Object.entries(exerciseData).map(([muscleGroup, exercises]) => (
+            <SubMenu label={muscleGroup} key={muscleGroup}>
+              <MenuItem disabled>Free Access</MenuItem>
+              {exercises
+                .filter((exercise) => exercise.access === "free")
+                .map((exercise) => (
                   <MenuItem
                     key={exercise.id}
-                    component={<Link to={`/${muscleGroup}/${exercise.id}`}/>}
+                    component={<Link to={`/${muscleGroup}/${exercise.id}`} />}
                   >
                     {exercise.name}
                   </MenuItem>
                 ))}
-              </SubMenu>
-            ))}
-          </div>
+              <MenuItem disabled>User Access</MenuItem>
+              {exercises
+                .filter((exercise) => exercise.access === "user")
+                .map((exercise) => (
+                  <MenuItem
+                    key={exercise.id}
+                    component={<Link to={`/${muscleGroup}/${exercise.id}`} />}
+                  >
+                    {exercise.name}
+                  </MenuItem>
+                ))}
+            </SubMenu>
+          ))}
         </div>
-      </Menu>
-    </Side>
+      </div>
+    </Menu>
+  </Side>
   );
 }
 
