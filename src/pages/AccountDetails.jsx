5075ec440d5ace1bf59833de7b10197f384f9b2f
Karl <toom.karl42@gmail.com>
2023-10-10 15:08:38 +0300

    added some styling to account details page

--- src/pages/AccountDetails.jsx
+++ src/pages/AccountDetails.jsx
@@ -43,33 +43,32 @@ function AccountDetails() {
       <div className="container">
         <div className="row">
           <div className="account__details">
-            <h1 className="accountr__details--header">AccountDetails</h1>
+            <h1 className="accountr__details--header">Account Details</h1>
             <div className="account__details--wrapper">
               <div className="detail">
                 <span className="detail-title">Email</span>
                 <span>{userData.email}</span>
+                <div className="line"></div>
               </div>
               <div className="detail">
                 <span className="detail-title">First Name</span>
-                <span>
-                  {userData.firstName} 
-                </span>
+                <span>{userData.firstName}</span>
+                <div className="line"></div>
               </div>
               <div className="detail">
                 <span className="detail-title">Last Name</span>
-                <span>
-                {userData.lastName}
-                </span>
+                <span>{userData.lastName}</span>
+                <div className="line"></div>
               </div>
               <div className="detail">
                 <span className="detail-title">Username</span>
-                <span>
-                  {userData.username}
-                </span>
+                <span>{userData.username}</span>
+                <div className="line"></div>
               </div>
               <div className="detail">
                 <span className="detail-title">Password</span>
-                <span>{authUser?.email}</span>
+                <span>Change your password</span>
+                <div className="line"></div>
               </div>
             </div>
           </div>
